19d879ca8d8c2a7c1d31dcbff8293001b1daa2fc
Tim Scott <0timothy.scott0@gmail.com>
2022-03-19 00:25:32 -0400

    connect to discord client

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 import 'dotenv/config';
 import { setOnMessage } from './logParser.js';
+import { Client, Intents } from 'discord.js';
+
+const prefix = '!';
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+    console.log('discord connection ready');
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.content.charAt(0) != prefix) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (!command || message.author.bot) return;
+    if(message.author !== client.user 
+        //with the below line commented it should respond to anyone
+        && message.author.id !== process.env.OWNERID
+    ) return;
+
+    if (command === 'ping') {
+        message.channel.send('pong!');
+    }
+
+});
 
 setOnMessage((message) => {
     // console.log(message);
@@ -12,3 +38,5 @@ setOnMessage((message) => {
     const clippedMessage = tmp.join('> ');
     console.log(`${name} => ${clippedMessage}`)
 });
+
+client.login(process.env.TOKEN);
