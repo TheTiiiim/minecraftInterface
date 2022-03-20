d2507a4e99f198a63cb2badf3f0d54a8b9646a77
Tim Scott <0timothy.scott0@gmail.com>
2022-03-19 21:35:18 -0400

    forward minecraft chat to set discord channel

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ import { Client, Intents } from 'discord.js';
 const prefix = '!';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+let guild;
+let channel;
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('discord connection ready');
+
+    if (process.env.GUILD && process.env.CHANNEL) {
+        guild = await client.guilds.fetch(process.env.GUILD);
+        channel = await guild.channels.fetch(process.env.CHANNEL);
+    }
 });
 
 client.on('messageCreate', async (message) => {
@@ -22,26 +29,46 @@ client.on('messageCreate', async (message) => {
         && message.author.id !== process.env.OWNERID
     ) return;
 
-    if (command === 'ping') {
-        message.channel.send('pong!');
+    switch (command) {
+        case 'ping':
+            message.channel.send('pong!');
+            break;
+        case 'list':
+            message.channel.send(await listPlayers());
+            break;
+        case 'setmcchannel':
+            if (!args[0]) break;
+            try {
+                channel = await message.guild.channels.fetch(args[0]);
+                guild = message.guild;
+                message.channel.send(`Set channel to "${args[0]}"`);
+            } catch (err) {
+                message.channel.send(`Could not set channel to "${args[0]}"`);
+            }
+            // message.channel.send(args[0]);
+            break;
     }
-
-    if (command === 'list') {
-        message.channel.send(await listPlayers());
-    }
-
 });
 
-setOnMessage((message) => {
-    // console.log(message);
-
+function mcParseChat(message) {
     const nameIndex = message.search(/<\w+>/);
     if (nameIndex === -1) return;
     let tmp = message.substr(nameIndex);
     tmp = tmp.split('> ');
-    const name = tmp.shift().substr(1);
+    const user = tmp.shift().substr(1);
     const clippedMessage = tmp.join('> ');
-    console.log(`${name} => ${clippedMessage}`)
+    return {user, message: clippedMessage};
+}
+
+setOnMessage((message) => {
+    let chatMessage = mcParseChat(message);
+
+    if (chatMessage && channel) {
+        channel.send(`(Minecraft) ${chatMessage.user}: ${chatMessage.message}`);
+        return;
+    }
+
+    console.log(message);
 });
 
 client.login(process.env.TOKEN);
